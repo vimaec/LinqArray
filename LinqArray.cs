62bcfa87dc06b5ab266feb73ab33f6330a17b244
Christopher Diggins <cdiggins@gmail.com>
2018-10-05 11:27:50 -0400

    Updating gitignore and adding a couple of functions

--- LinqArray.cs
+++ LinqArray.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Ara3D
 {
@@ -26,15 +27,13 @@ namespace Ara3D
         /// <summary>
         /// Accessor to the nth item in the array.
         /// </summary>
-        /// <param name="n"></param>
-        /// <returns></returns>
         T this[int n] { get; }
     }
 
     /// <summary>
     /// Implements an IArray via a function and a count. 
     /// </summary>
-    public class FunctionalArray<T> : IArray<T>
+    public struct FunctionalArray<T> : IArray<T>
     {
         public Func<int, T> Function { get; }
         public int Count { get; }
@@ -42,18 +41,30 @@ namespace Ara3D
         public FunctionalArray(int count, Func<int, T> function) { Count = count; Function = function; }
     }
 
+    /// <summary>
+    /// Extension functions for working on any object implementing IArray.
+    /// </summary>
     public static class ImmutableArray
     {
-        // Constructor functions 
+        /// <summary>
+        /// Returns an IArray from the list of arguments.
+        /// </summary>
+        public static IArray<T> Create<T>(params T[] args)
+        {
+            return args.ToIArray();
+        }
 
-        public static IArray<T> Create<T>(int count, Func<int, T> f)
+        /// <summary>
+        /// Creates an IArray with the given number of items, that uses the function to return items.
+        /// </summary>
+        public static IArray<T> Select<T>(this int count, Func<int, T> f)
         {
             return new FunctionalArray<T>(count, f);
         }        
 
         public static IArray<T> ToIArray<T>(this IList<T> self)
         {
-            return Create(self.Count, i => self[i]);
+            return Select(self.Count, i => self[i]);
         }
 
         public static IArray<T> ToIArray<T>(this IEnumerable<T> self)
@@ -64,7 +75,7 @@ namespace Ara3D
       
         public static IArray<T> Repeat<T>(this T self, int count)
         {
-            return Create(count, i => self);
+            return Select(count, i => self);
         }
 
         public static IArray<T> Unit<T>(this T self)
@@ -79,16 +90,28 @@ namespace Ara3D
 
         public static IArray<int> Range(this int n)
         {
-            return Create(n, i => i);
+            return Select(n, i => i);
         }
 
-        // Helper accessors
+        /// <summary>
+        /// Returns the nth item in the array. Undefined behavior if the array is empty. 
+        /// </summary>
+        public static T At<T>(this IArray<T> self, int n)
+        {
+            return self[n];
+        }
 
+        /// <summary>
+        /// Returns the first itme in the array. Undefined behavior if the array is empty. 
+        /// </summary>
         public static T First<T>(this IArray<T> self)
         {
             return self[0];
         }
 
+        /// <summary>
+        /// Returns the last itme in the array. Undefined behavior if the array is empty. 
+        /// </summary>
         public static T Last<T>(this IArray<T> self)
         {
             return self[self.Count - 1];
@@ -101,9 +124,7 @@ namespace Ara3D
         {
             return n >= 0 && n < self.Count;
         }
-
-        // 
-
+      
         /// <summary>
         /// A mnemonic for "Any()" that returns true if the count is greater than zero 
         /// </summary>
@@ -112,33 +133,57 @@ namespace Ara3D
             return self.Any();
         }
 
+        /// <summary>
+        /// Returns true if there are any elements in the array.
+        /// </summary>
         public static bool Any<T>(this IArray<T> self)
         {
             return self.Count == 0;
         }
 
+        /// <summary>
+        /// Returns true if there are any elements in the array that satisfy the predicate.
+        /// </summary>
         public static bool Any<T>(this IArray<T> self, Func<T, bool> func)
         {
             return self.ToEnumerable().Any(func);
         }
 
+        /// <summary>
+        /// Returns true if all elements in the array satisfy the predicate.
+        /// </summary>
         public static bool All<T>(this IArray<T> self, Func<T, bool> func)
         {
             return self.ToEnumerable().All(func);
         }
 
-        // Converters 
-
+        /// <summary>
+        /// Converts the IArray into a system array. 
+        /// </summary>
         public static T[] ToArray<T>(this IArray<T> self)
         {
             return self.CopyTo(new T[self.Count]);
         }
 
+        /// <summary>
+        /// Converts the IArray into a system list. 
+        /// </summary>
         public static List<T> ToList<T>(this IArray<T> self)
         {
             return self.AddTo(new List<T>(self.Count));
         }
 
+        /// <summary>
+        /// Forces an immediate evaluation of all items in the IArray, allocating memory, and storing all of the results. 
+        /// </summary>
+        public static IArray<T> Reify<T>(this IArray<T> self)
+        {
+            return self.ToArray().ToIArray();
+        }
+
+        /// <summary>
+        /// Converts an array into an IEnumerable. This is useful for using ForEach for example.
+        /// </summary>
         public static IEnumerable<T> ToEnumerable<T>(this IArray<T> self)
         {
             for (var i = 0; i < self.Count; ++i)
@@ -161,8 +206,10 @@ namespace Ara3D
             return self.ToFunction(false);
         }
 
-        // Copying
-
+        
+        /// <summary>
+        /// Adds all elements of the array to the target collection. 
+        /// </summary>
         public static U AddTo<T, U>(this IArray<T> self, U other) where U : ICollection<T>
         {
             foreach (var x in self.ToEnumerable())
@@ -170,6 +217,9 @@ namespace Ara3D
             return other;
         }
 
+        /// <summary>
+        /// Copies all elements of the array to the target list or array, starting at the provided index. 
+        /// </summary>
         public static U CopyTo<T, U>(this IArray<T> self, U other, int destIndex = 0) where U : IList<T>
         {
             for (var i = 0; i < self.Count; ++i)
@@ -177,14 +227,20 @@ namespace Ara3D
             return other;
         }
 
-        // Select (aka Map)
-
         /// <summary>
         /// Returns an array generated by applying a function to each element.
         /// </summary>
         public static IArray<U> Select<T, U>(this IArray<T> self, Func<T, U> f)
         {
-            return Create(self.Count, i => f(self[i]));
+            return Select(self.Count, i => f(self[i]));
+        }
+
+        /// <summary>
+        /// Converts an array of array into a flattened array. Each array is assumed to be of size n.
+        /// </summary>
+        public static IArray<T> Flatten<T>(this IArray<IArray<T>> self, int n)
+        {
+            return Select(self.Count * n, i => self[i / n][i % n]);
         }
 
         /// <summary>
@@ -192,7 +248,25 @@ namespace Ara3D
         /// </summary>
         public static IArray<U> Select<T, U>(this IArray<T> self, Func<T, int, U> f)
         {
-            return Create(self.Count, i => f(self[i], i));
+            return Select(self.Count, i => f(self[i], i));
+        }
+
+        /// <summary>
+        /// Converts an array of arrays to an enumerable.
+        /// </summary>
+        public static IEnumerable<T> SelectMany<T>(this IArray<IArray<T>> self)
+        {
+            foreach (var xs in self.ToEnumerable())
+                foreach (var x in xs.ToEnumerable())
+                    yield return x;
+        }
+
+        /// <summary>
+        /// Returns an array of arrays, where the number of sub-elements is known
+        /// </summary>
+        public static IArray<T> SelectMany<T>(this IArray<IArray<T>> self, int count)
+        {
+            return Select(self.Count, i => self[i / count][i % count]);
         }
 
         /// <summary>
@@ -200,7 +274,7 @@ namespace Ara3D
         /// </summary>
         public static IArray<V> Zip<T, U, V>(this IArray<T> self, IArray<U> other, Func<T, U, V> f)
         {
-            return Create(Math.Min(self.Count, other.Count), i => f(self[i], other[i]));
+            return Select(Math.Min(self.Count, other.Count), i => f(self[i], other[i]));
         }
 
         /// <summary>
@@ -208,7 +282,16 @@ namespace Ara3D
         /// </summary>
         public static IArray<V> Zip<T, U, V>(this IArray<T> self, IArray<U> other, Func<T, U, int, V> f)
         {
-            return Create(Math.Min(self.Count, other.Count), i => f(self[i], other[i], i));
+            return Select(Math.Min(self.Count, other.Count), i => f(self[i], other[i], i));
+        }
+
+        /// <summary>
+        /// Applies a function to each element in the list paired with the next one. 
+        /// Used to implement adjacnet differences for example.
+        /// </summary>
+        public static IArray<U> ZipEachWithNext<T, U>(this IArray<T> self, Func<T, T, U> f)
+        {
+            return self.DropLast().Zip(self.Skip(), f);
         }
 
         /// <summary>
@@ -297,17 +380,25 @@ namespace Ara3D
         /// </summary>
         public static IArray<T> Slice<T>(this IArray<T> self, int from, int to)
         {
-            return Create(to - from, i => self[i + from]);
+            return Select(to - from, i => self[i + from]);
+        }
+
+        /// <summary>
+        /// Returns n elements of the list starting from a given index.  
+        /// </summary>
+        public static IArray<T> Subarray<T>(this IArray<T> self, int from, int count)
+        {
+            return self.Slice(from, count + from);
         }
 
         public static IArray<T> Slice<T>(this IArray<T> self, int from, int to, int stride)
         {
-            return Create(to - from / stride, i => self[i * stride + from]);
+            return Select(to - from / stride, i => self[i * stride + from]);
         }
 
         public static IArray<T> Stride<T>(this IArray<T> self, int stride)
         {
-            return Create(self.Count / stride, i => self[i * stride % self.Count]);
+            return Select(self.Count / stride, i => self[i * stride % self.Count]);
         }
 
         public static IArray<T> Take<T>(this IArray<T> self, int n)
@@ -315,17 +406,17 @@ namespace Ara3D
             return self.Slice(0, n);
         }
 
-        public static IArray<T> Skip<T>(this IArray<T> self, int n)
+        public static IArray<T> Skip<T>(this IArray<T> self, int n = 1)
         {
             return self.Slice(n, self.Count);
         }
 
-        public static IArray<T> TakeLast<T>(this IArray<T> self, int n)
+        public static IArray<T> TakeLast<T>(this IArray<T> self, int n = 1)
         {
             return self.Skip(self.Count - n);
         }
 
-        public static IArray<T> DropLast<T>(this IArray<T> self, int n)
+        public static IArray<T> DropLast<T>(this IArray<T> self, int n = 1)
         {
             return self.Take(self.Count - n);
         }
@@ -355,9 +446,12 @@ namespace Ara3D
         /// </summary>
         public static IArray<T> Resize<T>(this IArray<T> self, int count)
         {
-            return Create(count, i => self[i % self.Count]);
+            return Select(count, i => self[i % self.Count]);
         }
 
+        /// <summary>
+        /// Returns an array of the same type with no elements. 
+        /// </summary>
         public static IArray<T> Clear<T>(this IArray<T> self)
         {
             return self.Take(0);
@@ -369,6 +463,55 @@ namespace Ara3D
         public static IArray<int> Indices<T>(this IArray<T> self)
         {
             return self.Count.Range();
+        }
+
+        /// <summary>
+        /// Converts an array of elements into a string representation
+        /// </summary>
+        public static string Join<T>(this IArray<T> self, string sep = " ")
+        {
+            return self.Aggregate(new StringBuilder(), (sb, x) => sb.Append(x).Append(sep)).ToString();
+        }
+
+        /// <summary>
+        /// Concatenates the contents of one array with another.
+        /// </summary>
+        public static IArray<T> Concatenate<T>(this IArray<T> self, IArray<T> other)
+        {
+            return Select(self.Count + other.Count, i => i < self.Count ? self[i] : other[i - self.Count]);
+        }
+    
+        /// <summary>
+        /// Returns the index of the first element matching the given item.
+        /// </summary>
+        public static int IndexOf<T>(this IArray<T> self, T item) where T : IEquatable<T>
+        {
+            for (var i = 0; i < self.Count; ++i)
+                if (self[i].Equals(item))
+                    return i;
+            return -1;
+        }
+
+        // TODO: CountWhere, and IndexOf with predicate, Stats, Min, and Max, Apply Edits (change values ... based on)
+        public static IArray<int> AdjacentDifferences(this IArray<int> self)
+        {
+            return self.ZipEachWithNext((a, b) => b - a);
+        }
+
+        /// <summary>
+        /// Creates a new array that concatenates a unit item list of one item after it.       
+        /// </summary>
+        public static IArray<T> Append<T>(this IArray<T> self, T x)
+        {
+            return self.Concatenate(x.Unit());
+        }
+
+        /// <summary>
+        /// Creates a new array that concatenates a unit item list of one item before it   
+        /// </summary>
+        public static IArray<T> Prepend<T>(this IArray<T> self, T x)
+        {
+            return x.Unit().Concatenate(self);
         }
     }
 }
