3bf33aa2120bb748eae2c71f7937021bf29c356e
Christopher Diggins <cdiggins@gmail.com>
2018-12-17 10:25:16 -0500

    Moving to .NET 4.7. Restructuring project.

--- LinqArrayTests/Properties/AssemblyInfo.cs
+++ LinqArrayTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LinqArray.Tests")]
+[assembly: AssemblyTitle("LinqArrayTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LinqArray.Tests")]
+[assembly: AssemblyProduct("LinqArrayTests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7ff17caa-8f78-47cf-8499-3923f652e9d7")]
+[assembly: Guid("b83d1fbe-6708-41eb-a028-d604cb44a5c1")]
 
 // Version information for an assembly consists of the following four values:
 //
