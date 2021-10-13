82c99c55351a8146b9070f9f54022a2e3f85e345
Vadim Vorotilin <vadim.vorotilin@gmail.com>
2021-10-13 16:09:53 +0300

    PR updates

--- LinqArray.Tests/LinqArrayTests.cs
+++ LinqArray.Tests/LinqArrayTests.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace Vim.LinqArray.Tests
 {
-    public class LinqArrayTestss
+    public class LinqArrayTests
     {
         public static int[] ArrayToTen = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         public static IArray<int> RangeToTen = 10.Range();
